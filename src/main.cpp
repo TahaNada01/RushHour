4b28fa5f011d51fdefd238a67554ac406c5987c8
p2207510 <nada.taha@etu.univ-lyon1.fr>
2023-03-07 16:36:11 +0100

    ss

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,8 @@
 #include "vehicule.hpp"
 
 int main(){
-    
-    Grille grille("rush-hour-etu/Sujet/puzzle.txt");
-    grille.print(); // Affiche la grille
-    return 0;
 
-}
-
-// Création d'un véhicule
+    // Création d'un véhicule
     /*Vehicule voiture(2, 3, 4, true);
 
     // Affichage des caractéristiques
@@ -21,3 +15,10 @@ int main(){
     std::cout << "Colonne : " << voiture.getColonne() << std::endl;
     std::cout << "Longueur : " << voiture.getLongueur() << std::endl;
     std::cout << "Orientation horizontale : " << voiture.Horizantale() << std::endl;*/
+    
+    Grille grille("../Sujet/puzzle.txt");
+    grille.print(); // Affiche la grille
+
+    return 0;
+
+}
